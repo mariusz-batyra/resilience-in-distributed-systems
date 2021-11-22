92e4097436d38348441f2d0c98660488474b75fd
Mariusz Batyra <mariusz.batyra@allegro.pl>
2021-11-22 20:32:19 +0100

    Demo 2

--- api/src/main/kotlin/pl/mbatyra/api/ApiApplication.kt
+++ api/src/main/kotlin/pl/mbatyra/api/ApiApplication.kt
@@ -2,11 +2,8 @@ package pl.mbatyra.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
-import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
-@EnableFeignClients
 class ApiApplication
 
 fun main(args: Array<String>) {
--- external-service/src/main/kotlin/pl/mbatyra/externalservice/InvoiceEndpoint.kt
+++ external-service/src/main/kotlin/pl/mbatyra/externalservice/InvoiceEndpoint.kt
@@ -16,27 +16,6 @@ class InvoiceEndpoint {
     @GetMapping("/demo2/invoices/{userId}")
     fun getUserInvoicesDemo2(@PathVariable userId: String): ResponseEntity<UserInvoicesDto> {
         logger.info("Rest request to get user $userId invoices")
-        Thread.sleep(10 * 1000L) // 10 sec
-        return INVOICES[userId]?.let {
-            ResponseEntity.ok(UserInvoicesDto(it))
-        } ?: ResponseEntity.notFound().build()
-    }
-
-    @GetMapping("/demo3/invoices/{userId}")
-    fun getUserInvoicesDemo3(@PathVariable userId: String): ResponseEntity<UserInvoicesDto> {
-        logger.info("Rest request to get user $userId invoices")
-        return INVOICES[userId]?.let {
-            ResponseEntity.ok(UserInvoicesDto(it))
-        } ?: ResponseEntity.notFound().build()
-    }
-
-    @GetMapping("/demo4/invoices/{userId}")
-    fun getUserInvoicesDemo4(@PathVariable userId: String): ResponseEntity<UserInvoicesDto> {
-        logger.info("Rest request to get user $userId invoices")
-        // val timeout = Random().nextInt(600)
-        val timeout = 1000L
-        logger.info("applied timeout: $timeout ms")
-        Thread.sleep(timeout.toLong())
         return INVOICES[userId]?.let {
             ResponseEntity.ok(UserInvoicesDto(it))
         } ?: ResponseEntity.notFound().build()
